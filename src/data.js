70aa966c778ab5f7c8011ac4c749e12ebd8b1d65
Indraan S. Toor <indraantoor@gmail.com>
2021-12-05 02:39:48 +0530

    Add: product listings components

--- src/data.js
+++ src/data.js
@@ -67,7 +67,7 @@ export const popularProducts = [
   },
   {
     id: 7,
-    img: "https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+    img: "https://ibb.co/ygzbdRV",
   },
   {
     id: 8,
