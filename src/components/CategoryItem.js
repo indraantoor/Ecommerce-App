42a70cd34ffdc5c45f68d651aa46aca17250ecdb
Indraan S. Toor <indraantoor@gmail.com>
2021-12-05 19:37:31 +0530

    Fix: made homepage responsive

--- src/components/CategoryItem.js
+++ src/components/CategoryItem.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { mobile } from "../responsive";
 
 const Container = styled.div`
   flex: 1;
@@ -6,11 +7,14 @@ const Container = styled.div`
   height: 70vh;
   position: relative;
 `;
+
 const Image = styled.img`
   width: 100%;
   height: 100%;
   object-fit: cover;
+  ${mobile({ height: "30vh" })}
 `;
+
 const Info = styled.div`
   position: absolute;
   top: 0;
@@ -22,10 +26,12 @@ const Info = styled.div`
   align-items: center;
   justify-content: center;
 `;
+
 const Title = styled.h1`
   color: white;
   margin-bottom: 20px;
 `;
+
 const Button = styled.button`
   border: none;
   padding: 10px;
