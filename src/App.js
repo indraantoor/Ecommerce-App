44415aaf05688103ccdeac3f95fd6b9cbec054e0
Indraan S. Toor <indraantoor@gmail.com>
2021-12-05 18:19:31 +0530

    Add: login and register page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import "./App.css";
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 import Product from "./pages/Product";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
-  return <Product />;
+  return <Login />;
 }
 
 export default App;
