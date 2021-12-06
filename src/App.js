5c9e3c4225847cb6d54d227a800c2226d24d58c0
Indraan S. Toor <indraantoor@gmail.com>
2021-12-07 01:58:53 +0530

    Add: routing to home components

--- src/App.js
+++ src/App.js
@@ -5,9 +5,31 @@ import Product from "./pages/Product";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
-  return <Home />;
+  const user = true;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/products/:category" element={<ProductList />} />
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+        <Route
+          path="/register"
+          element={user ? <Navigate to="/" /> : <Register />}
+        />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
